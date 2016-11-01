89885a4f0175b98391db5ec24f9e1b5e9837859f
Jenny Adams <jennyleighadams@gmail.com>
2016-11-01 14:54:40 +1300

    composer does autoloading of classes

--- index.php
+++ index.php
@@ -4,13 +4,6 @@
 
 	error_reporting(E_ALL);
 
-	//autoloadting function for classes
-	function my_autoloader($class){
-		require "classes/". $class .".php";
-
-	}
-	spl_autoload_register('my_autoloader');
-
 	session_start();
 
 	require "vendor/autoload.php";
